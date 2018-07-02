1b5b7699bd3e77d9a8df8b67a8c19ebe0d60c84b
katrink66 <39248380+katrink66@users.noreply.github.com>
2018-07-02 05:27:22 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,50 +6,38 @@
 function makeGrid() {
 
 // Your code goes here!
-$(function() {
-  //Set the grid and current color to #000
-  var theGrid = $("#pixel_canvas");
-  var currentColor = "#000";
 
-  // Prevent default submit behavior and call makeGrid function
-  $("#sizePicker").submit(function(e) {
-    e.preventDefault();
-    makeGrid();
-  });
+  
+//define rows, columns
+  var rows = $("#inputHeight").val();
+  var columns = $("#inputWeight").val();
 
-  //Change the currentColor to the selected one
-  $("#colorPicker").change(function() {
-    currentColor = $(this).val();
-  });
+  //define table
+  var table = $("#pixelCanvas");
 
-  //If the user clicks or moves the mouse in the grid
-  //while holding the left mouse button down draw the pixels
-  theGrid.on("mousemove", "td", function(e) {
-    if (e.buttons == 1) {
-      $(this).css("background-color", currentColor); //Draw the td that the user hovers
-    }
-  });
+  //Remove previous table
+  table.children().remove();
 
-  theGrid.on("click", "td", function(e) {
-    $(this).css("background-color", currentColor); //Draw the td that the user clicks
-  });
-
-  //Build the grid
-  function makeGrid() {
-    //Set the grid dimensions;
-    var gridHeight = $("#input_height").val();
-    var gridWidth = $("#input_width").val();
-    theGrid.empty(); //Empty the grid and start over
-    //Loop -> Create rows
-    for (x = 0; x <= gridHeight - 1; x++) {
-      theGrid.append("<tr>"); //Start Row
-      //Loop -> Create columns for each row
-      for (y = 0; y <= gridWidth - 1; y++) {
-        $("tr:eq(" + x + ")").append("<td></td>");
-      }
-      theGrid.append("</tr>"); //End Row
+  //Create rows
+  for (var i = 0; i <= rows; i++) {
+    table.append("<tr></tr>");
+    
+    //Create columns
+    for (var j = 0; j <= columns; j++) {
+      table.children().last().append("<td></td>");
     }
   }
-});
 
+  table.on("click", "td", function() {
+    //choose color
+    var color = $("input[type='color']").val();
+    //Apply color 
+    $(this).attr("bgcolor", color);
+  });
 }
+
+// Event Listener for button clicks to trigger makeGrid()
+$("input[type='submit']").click(function(e) {
+  e.preventDefault(); 
+  makeGrid(); //Create table
+});
